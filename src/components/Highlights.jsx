393ff6498469e2f21a326e65143b1460db779f84
Subh <subhaushsingh@gmail.com>
2025-06-05 22:01:03 +0530

    highlight components and video section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { rightImg, watchImg } from '../utils/utils';
+import Videocaraosel from './Videocaraosel';
 
 const Highlights = () => {
+
+  useGSAP(()=>{
+    gsap.to('#title',{opacity:1,y:0})
+    gsap.to('.link',{opacity:1,y:0,duration:1,stagger:0.25})
+  },[])
+
   return (
-    <div>Highlights</div>
+    <section id='highlights'className='w-screen overflow-hidden h-full common-padding bg-zinc-950'>
+      <div className='screen-max-width'>
+        <div className='mb-12 w-full items-end md:flex justify-between'>
+          <h1 id='title' className='section-heading'>Get the highlights.</h1>
+          <div className='flex flex-wrap items-end gap-5'>
+            <p className='link flex gap-1'>watch the film 
+            <img src={watchImg} alt='watch' className='ml-1 '/>
+            </p> 
+            <p className='link flex gap-1'>watch the event 
+            <img src={rightImg} alt='watch' className='ml-1'/>
+            </p> 
+          </div>
+        </div>
+        <Videocaraosel />
+      </div>
+    </section>
   )
 }
 
