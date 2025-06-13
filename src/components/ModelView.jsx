6c0dddcde3d85d05ccf0db61039179d946ed36fe
Subh <subhaushsingh@gmail.com>
2025-06-13 22:25:47 +0530

    Model setup

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { PerspectiveCamera, View } from '@react-three/drei'
+import React, { Suspense } from 'react'
+import Lights from './Light'
+import IPhone from './IPhone'
 
-const ModelView = () => {
+const ModelView = ({index,groupRef,gsapType,controlRef,setRotationState,item,size}) => {
   return (
-    <div>ModelView</div>
+    <View
+    index={index}
+    id={gsapType}
+    className={`border-2 border-red-500 w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}    >
+
+    <ambientLight intensity={0.3} />
+    <PerspectiveCamera  makeDefault position={[0,0,4]}/>
+
+    <Suspense fallback={<div>Loading...</div>}>
+      
+    </Suspense>
+
+    </View>
   )
 }
 
--- src/components/Models.jsx
+++ src/components/Models.jsx
@@ -81,12 +81,12 @@ const Models = () => {
             <div className='flex-center'>
               <ul className='color-container'>
                 {models.map((item,i)=>(
-                   <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{ backgroundColor: item.color[0] }} onClick={() => setModel(item)} />
+                  <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{ backgroundColor: item.color[0] }} onClick={() => setModel(item)} />
                 ))}
               </ul>
               <button className='size-btn-container'>
                 {sizes.map(({label,value})=>(
-                  <span key={label} className='size-btn'>{label}</span>
+                  <span key={label} className='size-btn' style={{backgroundColor:size===value ? 'white':'transparent',color:size===value?'black':'white'}} onClick={()=>setSize(value)}>{label}</span>
                 ))}
               </button>
             </div>
